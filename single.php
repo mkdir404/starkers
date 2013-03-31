a96b1e3591dd3490a5c2cc15e668647672475807
mkdir404 <mkdir404@gmail.com>
2013-03-31 17:28:19 -0600

    remove header and foot and fix modal

--- single.php
+++ single.php
@@ -9,7 +9,6 @@
  * @since 		Starkers 4.0
  */
 ?>
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' , 'parts/shared/sidebar'  ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
@@ -73,6 +72,3 @@
 
 
 </div> <!-- end .pagecontent -->
-
-
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
